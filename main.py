80f9d72d2d0660c6766d7ccf5b54a762d665c330
Benjamin Brahmer <info@b-brahmer.de>
2017-08-07 17:51:42 +0200

    custom name for each button

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 from tkinter import filedialog as fd
+from tkinter import simpledialog as sd
 import tkinter as tk
 from functools import partial
 from playsound import playsound
@@ -19,8 +20,10 @@ def playSound(filename):
 def genButton():
     root.filename = fd.askopenfilename(initialdir="~/Musik", title="Select file",
                                        filetypes=(("audio files", "*.mp3"), ("audio files", "*.wav")))
-    button = tk.Button(root, text="lol", command=partial(playSound, root.filename))
-    button.pack()
+    root.result = sd.askstring('Name', 'Enter a name for the button')
+    if(len(root.filename) > 0):
+        button = tk.Button(root, text=root.result, command=partial(playSound, root.filename))
+        button.grid()
 
 
 def main():
@@ -28,7 +31,8 @@ def main():
     root.config(menu=menu)
     menu.add_command(label="New", command=genButton)
     w = tk.Label(root, text="Hello World")
-    w.pack()
+    w.grid()
+    root.geometry('400x400')
     root.mainloop()
 
 
